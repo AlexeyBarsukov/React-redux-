dd03e0c65506d27cd30b3035948038ffe327db0d
Alexey <alexeyBarsAlexey@mail.ru>
2022-06-27 23:42:29 +0300

    added reducers changed structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,53 @@
-import './App.css';
+import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash.cash)
-  console.log(cash)
+  const stateCash = useSelector(state => state.cash.cash)
+  const customerState = useSelector(state => state.customers.customer)
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash })
   }
-
   const getCash = (cash) => {
     dispatch({ type: "GET_CASH", payload: cash })
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
-      <div style={{ margin: '30px', fontSize: '20px' }}>{cash}</div>
-      <button onClick={() => addCash(Number(prompt()))}>Пополнить счёт</button>
-      <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
+      <div style={{ fontWeight: 'Bold', fontFamily: 'Roboto', fontSize: '50px', }}>{stateCash}</div>
+      <div style={{ margin: '20px' }}>
+        <Button style={{ marginRight: '10px' }} onClick={() => addCash(Number(prompt()))}>Пополнить счёт</Button>
+        <Button onClick={() => getCash(Number(prompt()))}>Снять со счёта</Button>
+        <div style={{ margin: '20px' }}>
+          <Button variant="secondary" style={{ marginRight: '10px' }} onClick={() => addCustomer(prompt())}>Добавить клиента</Button>
+          <Button variant="secondary" onClick={null}>Удалить клиента</Button>
+        </div>
+      </div>
+      <div>
+        {customerState.length > 0 ?
+          <div>
+            {customerState.map(customer => <div onClick={() => removeCustomer(customer)} style={{ fontWeight: 'Bold', fontFamily: 'Roboto', fontSize: '50px', }}>{customer.name}</div>)}
+          </div> :
+          <div>
+            Клиенты отсутствуют
+          </div>}
+      </div>
     </div>
   );
 }
--- my-app/src/store/customerReducer.js
+++ my-app/src/store/customerReducer.js
@@ -1,16 +1,20 @@
 const defaultState = {
-  customer: ['1']
+  customer: [],
 }
 
+const ADD_CUSTOMER = 'ADD_CUSTOMER';
+const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS';
 
 export const customerReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_CUSTOMER":
-      return { ...state, }
-    case "GET_CUSTOMERS":
-      return { ...state, }
+    case ADD_CUSTOMER:
+      return { ...state, customer: [...state.customer, action.payload] }
+    case REMOVE_CUSTOMERS:
+      return { ...state, customer: state.customer.filter(customer => customer.id !== action.payload) }
     default:
       return state
   }
-
 }
+
+export const addCustomerAction = (payload) => ({ type: ADD_CUSTOMER, payload })
+export const removeCustomerAction = (payload) => ({ type: REMOVE_CUSTOMERS, payload })
